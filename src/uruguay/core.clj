66567a3170b9d684b36bce9ee721456b2770033e
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-23 22:30:01 -0300

    Add section on reference types

--- src/uruguay/core.clj
+++ src/uruguay/core.clj
@@ -46,31 +46,62 @@ println
 ;; === Immutable data structures ===
 
 (def user {:name "Ben" :age 31})
+(identity user)
 
 (assoc user :city "Boston")
-(dissoc user :name)
+(identity user)
 
+(dissoc user :name)
+(identity user)
+
+
+
+;; === When things "change" ===
+
+(def employee-count (atom 1))
+
+(swap! employee-count inc)
+
+
+(def savings-account (ref 100))
+(def checking-account (ref 100))
+
+(defn move-from-checking-to-savings
+  [checking-account
+   savings-account
+   amount]
+  (dosync
+    (alter checking-account - amount)
+    (alter savings-account + amount)))
+
+(move-from-checking-to-savings
+  checking-account
+  savings-account
+  10)
+
+(println
+  "Savings balance: " @savings-account
+  "Checking balance: " @checking-account)
 
 
 ;; === Macros ===
 
 (defmacro using-postfix
   [expression]
-  (conj (butlast expression)
-        (last expression)))
+  (cons
+    (last expression)
+    (butlast expression)))
 
 (using-postfix (1 2 +))
 
 
 (defmacro unless
   [test then else]
-  `(if (not ~test) ~then ~else))
+  (if (not test) then else))
 
 (unless false "then" "else")
 (unless true "then" "else")
 
-
-;; === When things "change"  ===
 
 
 
